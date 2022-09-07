711d7ca02787dda9afbc89792c2f5f3b6d1e7d61
Bhoomil Dayani <bhoomildayani182@gmail.com>
2022-09-07 22:15:24 +0530

    Fetching News category wise (connect reacr-router-dom )

--- src/App.js
+++ src/App.js
@@ -2,12 +2,24 @@
 import './App.css';
 import NavigationBar from './Component/NavigationBar';
 import News from './Component/News';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 function App() {
   return (
     <div>
+      <Router>
       <NavigationBar />
-      <News pageSize={6} country="in" category="business" />
+      <Routes>
+          <Route path="home" element={<News pageSize={6} country="in" category="general" />} />
+          <Route path="business" element={<News pageSize={6} country="in" category="business" />} />
+          <Route path="entertainment" element={<News pageSize={6} country="in" category="entertainment" />} />
+          <Route path="general" element={<News pageSize={6} country="in" category="general" />}/>
+          <Route path="health" element={<News pageSize={6} country="in" category="health" />} />
+          <Route path="science" element={<News pageSize={6} country="in" category="science" />} />
+          <Route path="sport" element={<News pageSize={6} country="in" category="sport" />} />
+          <Route path="technology" element={<News pageSize={6} country="in" category="technology"></News>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/Component/NavigationBar.js
+++ src/Component/NavigationBar.js
@@ -1,24 +1,23 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import ListGroup from 'react-bootstrap/ListGroup';
 
 function NavigationBar() {
   return (
     <>
       <Navbar bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">NewsWeb</Navbar.Brand>
+          <Navbar.Brand href="">NewsWeb</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">About</Nav.Link>
-            <Nav.Link href="#features">Business</Nav.Link>
-            <Nav.Link href="#features">Entertainment</Nav.Link>
-            <Nav.Link href="#features">General</Nav.Link>
-            <Nav.Link href="#features">Health</Nav.Link>
-            <Nav.Link href="#features">Science</Nav.Link>
-            <Nav.Link href="#features">Sport</Nav.Link>
-            <Nav.Link href="#features">Technology</Nav.Link>
+            <Nav.Link href="/home">Home</Nav.Link>
+            <Nav.Link href="/About">About</Nav.Link>
+            <Nav.Link href="/business">Business</Nav.Link>
+            <Nav.Link href="/entertainment">Entertainment</Nav.Link>
+            <Nav.Link href="/general">General</Nav.Link>
+            <Nav.Link href="/health">Health</Nav.Link>
+            <Nav.Link href="/science">Science</Nav.Link>
+            <Nav.Link href="/sport">Sport</Nav.Link>
+            <Nav.Link href="/technology">Technology</Nav.Link>
      </Nav>
         </Container>
       </Navbar>
